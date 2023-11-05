9a2d15e0991a98fe57d2c72cf256d273f3bddaee
arifalhidayah <78639582+arifalhidayah@users.noreply.github.com>
2023-11-05 11:54:17 +0700

    add Web Storage

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
         todos.push(todoObject);
 
         document.dispatchEvent(new Event(RENDER_EVENT));
+        saveData();
     }
 
     function generateId() {
@@ -105,6 +106,7 @@ document.addEventListener('DOMContentLoaded', function() {
         todoTarget.isCompleted = true;
 
         document.dispatchEvent(new Event(RENDER_EVENT));
+        saveData();
     }
 
     function removeTaskFromCompleted(todoId) {
@@ -114,6 +116,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         todos.splice(todoTarget, 1)
         document.dispatchEvent(new Event(RENDER_EVENT));
+        saveData();
     }
 
     function undoTaskFromCompleted(todoId) {
@@ -123,6 +126,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         todoTarget.isCompleted = false;
         document.dispatchEvent(new Event(RENDER_EVENT));
+        saveData();
     }
 
     function findTodo(todoId) {
@@ -145,5 +149,44 @@ document.addEventListener('DOMContentLoaded', function() {
         return -1;
     }
 
+    const SAVED_EVENT = 'saved-todo';
+    const STORAGE_KEY = 'TODO_APPS';
+
+    function isStorageExist() {
+        if (typeof (Storage) === undefined) {
+            alert('Browser kamu tidak mendukung local storage')
+            return false;
+        }
+        return true;
+    }
+
+    document.addEventListener(SAVED_EVENT, function () {
+        console.log(localStorage.getItem(STORAGE_KEY));
+    });
+
+    function saveData(){
+        if (isStorageExist()) {
+            const parsed = JSON.stringify(todos);
+            localStorage.setItem(STORAGE_KEY, parsed);
+            document.dispatchEvent(new Event(SAVED_EVENT));
+        }
+    }
+
+    function loadDataFromStorage() {
+        const serializedData = localStorage.getItem(STORAGE_KEY);
+        let data = JSON.parse(serializedData);
+
+        if (data !== null) {
+            for (const todo of data) {
+                todos.push(todo);
+            }
+        }
+
+        document.dispatchEvent(new Event(RENDER_EVENT));
+    }
+
+    if (isStorageExist()) {
+        loadDataFromStorage();
+    } 
     
 });
